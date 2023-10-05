4489a51be386a2669051a79ce643f54f5df04c02
SHeyNyAndrew <146359177+SHeyNyAndrew@users.noreply.github.com>
2023-10-05 16:21:17 +0000

    прррррр

---
+++ lab_1_task_1.py
@@ -0,0 +1,5 @@
+print('Hello, World!')
+print('Маша + Петя = любовь')
+print('х = 3 + 4')
+x = 3 + 4
+print('x=', x)
---
+++ lec_1_input.py
@@ -0,0 +1,10 @@
+a = input()
+print(a)
+
+a = input('что значит голубой: ')
+print(a)
+print(type(a))
+
+a = int(input('число дай'))
+print(a)
+print(type(a))
