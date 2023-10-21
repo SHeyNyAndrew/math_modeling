1f8083a1e98c0bc2229548f54eddfc51814455ff
SHeyNyAndrew <146359177+SHeyNyAndrew@users.noreply.github.com>
2023-10-21 11:02:52 +0000

    hgh

--- lab_2_task_5.py
+++ lab_2_task_5.py
@@ -1,3 +1,8 @@
 a = int(input("введите число"))
 b = int(input("введите число"))
-if a % b ==0
+if a%b == 0:
+    print("%d делится на %d" % (a,b))
+else:
+    print("%d не делится на %d" % (a,b))
+    print("Остаток: %d" % (a%b))
+print("Частное: %d" % (a//b))
