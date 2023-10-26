a9faa8b226dc60d7d2e5e0ecc8591f598ed4315a
SHeyNyAndrew <146359177+SHeyNyAndrew@users.noreply.github.com>
2023-10-26 16:37:31 +0000

    jh

---
+++ lec_3_math.py
@@ -0,0 +1,12 @@
+import math
+a = math.sin(3 * math.pi / 5)
+print(a)
+
+b=math.log10(42)
+print(b)
+
+c = math.sqrt(3)
+print(c)
+
+d = math.asin(math.tan(math.pi / 4)) * 180 / math.pi
+print(d)
---
+++ lec_3_my_module.py
@@ -0,0 +1,6 @@
+a = 3
+b ='good'
+c=['bad', 4, 'Andrusha molodec']
+earth_mass=5.97237 * 10**30
+sigma_steff_bolc = 5.67036713 *10 **(-8)
+gracity_constant =6.67408 * 10 **(-11)
