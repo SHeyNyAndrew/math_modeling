ac59a91fcd953fb4a386631f4871893ead162d4c
SHeyNyAndrew <146359177+SHeyNyAndrew@users.noreply.github.com>
2023-11-02 16:14:52 +0000

    hh

--- lab_3_task_2.py
+++ lab_3_task_2.py
@@ -6,3 +6,11 @@ b =p / 6
 h = 100 
 v=np.sqrt((g * h * (np.tan(b)**2)) / (2*(np.cos(a) **2)*  (1 - (np.tan(b) * (np.tan(a))))))
 print(v)
+
+
+
+from lab_3_task_1 import k, e, h
+t = 200 
+p = 3.14
+ε = 300
+n=(2 / np.sqrt(p)) * (h / ((k*t)**3/2)) *(e ** (-ε/(k*t))) * (ε**(t/2))
