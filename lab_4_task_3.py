2e4997ef9a52043bc771423619fe72b45ab17a57
SHeyNyAndrew <146359177+SHeyNyAndrew@users.noreply.github.com>
2023-11-30 16:25:31 +0000

    yuu8yu7gyu

--- lab_4_task_3.py
+++ lab_4_task_3.py
@@ -1,12 +1,14 @@
 import numpy as np
-m=50
-h=30
-v=10
+
 g=9.8
 
-def energy(m, h, v, g):
+def energy(m, h, v):
     Ek = (m * (v ** 2)) / 2
     Ep = m * g * h
     energy = Ek * Ep
     return energy
-print(energy)
+
+print(energy(3, 6, 9))
+
+e = energy(31, 64, 9)
+print(e)
