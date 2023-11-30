53a4a3c3200ff042aa329b10f0aba68b75460f4c
SHeyNyAndrew <146359177+SHeyNyAndrew@users.noreply.github.com>
2023-11-30 14:28:56 +0000

    jhhj

---
+++ lec_4_import_function.py
@@ -0,0 +1,14 @@
+from lec_4_create_funcion import muit_func
+from lec_4_create_funcion import print_func
+
+tmp = muit_func(4)
+
+print (tmp)
+
+print (muit_func(10))
+
+print(muit_func('Good'))
+
+print_func('Hello')
+
+print_func(muit_func('50'))
