3cd9c8719646448700e3baf7832c219050990fb4
SHeyNyAndrew <146359177+SHeyNyAndrew@users.noreply.github.com>
2023-12-07 12:29:44 +0000

    yhhj

---
+++ lab_4_task_4.py
@@ -0,0 +1,3 @@
+import numpy as np
+def function (a, b):
+    x=
---
+++ lab_4_task_5.py
@@ -0,0 +1,13 @@
+def area(figure, data):
+    if figure == 'круг':
+        print(data[0])
+        res = 3.14*(data[0]**2)
+    if figure == 'квадрат':
+        a,b = data
+        res = a*b
+    if figure == 'треугольник':
+        res = (a*b)/2
+    return (res)
+    
+figure = input('фигура ')
+print(area(figure))
